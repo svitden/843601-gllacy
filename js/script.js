62df59ce0412d8b4145d17ab247392fca581efb5
svitden <svetlana.denisenko@gmail.com>
2018-09-17 21:21:31 +0300

    Что-то еще

--- js/script.js
+++ js/script.js
@@ -41,7 +41,7 @@ closeFeedback.addEventListener("click", function (evt) {
 });
 
 form.addEventListener("submit", function (evt) {
-  if (!userName.value || !feedbackEmail.value || !commentField) {
+  if (!userName.value || !feedbackEmail.value || !commentField.value) {
     evt.preventDefault();
     popupFeedback.classList.remove("modal-error");
     popupFeedback.offsetWidth = popupFeedback.offsetWidth;
